c17de03b225912d2bb54d00f6e04e11187d96f70
E. Almqvist <elalmqvist@gmail.com>
2020-12-08 09:34:56 +0100

    Added button selection

---
+++ js/foodbutton.js
@@ -0,0 +1,22 @@
+var buttons = Array.from(document.getElementsByClassName("foodbutton"));
+console.log(buttons);
+
+// Kinda the same code os terminal.js but this isn't written to be very portable.
+
+function switchSel(elem, newcl) {
+    elem.className.replace( /\foodselected\b/g, newcl );
+}
+
+for ( let i = 0; i<buttons.length; i++ ) {
+	let btn = buttons[i];
+
+	btn.addEventListener("click", function() {
+	    console.log(btn.parentElement.className);
+
+	    [].forEach.call(buttons, function(el) {
+		el.parentElement.className = el.parentElement.className.replace(/\bfoodselected\b/, "");
+	    });
+
+	    btn.parentElement.classList.add("foodselected");
+	});
+}
