7de9ccc3b61e2bfd1c65ae496b4cfb88201a6a71
E. Almqvist <elalmqvist@gmail.com>
2020-12-08 17:55:02 +0100

    Indent fix

--- js/foodbutton.js
+++ js/foodbutton.js
@@ -5,19 +5,19 @@ var foodheader = document.getElementById("foodcontents_header");
 // Kinda the same code os terminal.js but this isn't written to be very portable.
 
 function switchSel(elem, newcl) {
-    elem.className.replace( /\foodselected\b/g, newcl );
+	elem.className.replace( /\foodselected\b/g, newcl );
 }
 
 for ( let i = 0; i<buttons.length; i++ ) {
 	let btn = buttons[i];
 
 	btn.addEventListener("click", function() {
-	    [].forEach.call(buttons, function(el) {
-		el.parentElement.className = el.parentElement.className.replace(/\bfoodselected\b/, "");
-	    });
+		[].forEach.call(buttons, function(el) {
+			el.parentElement.className = el.parentElement.className.replace(/\bfoodselected\b/, "");
+		});
 
-	    btn.parentElement.classList += " foodselected";
+		btn.parentElement.classList += " foodselected";
 
-	    foodheader.innerText.replace(/([0-9]+)/, toString(i));
+		foodheader.innerText.replace(/([0-9]+)/, toString(i));
 	});
 }
